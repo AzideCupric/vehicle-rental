032423f493471823104f199452968ab1c51cb2e8
Azide <rukuy@qq.com>
2022-05-04 20:12:20 +0800

    微调路由

--- rentalos/__init__.py
+++ rentalos/__init__.py
@@ -15,7 +15,7 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
-    @app.route('/')
+    @app.route('/hello')
     def hello():
         return 'Hello,World'
     
