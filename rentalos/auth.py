1ba112aff9305e1cb1ae1e9a93d8b5526b6748fa
Azide <rukuy@qq.com>
2022-05-06 00:41:53 +0800

    初步实现了添加数据功能

--- rentalos/auth.py
+++ rentalos/auth.py
@@ -1,90 +1,99 @@
 import functools
-from textwrap import indent
-from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for
-from werkzeug.security import generate_password_hash, check_password_hash
-from rentalos.db import get_db
+
+from flask import (
+    Blueprint,
+    flash,
+    g,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
+)
 from tinydb import where
+from werkzeug.security import check_password_hash, generate_password_hash
 
-auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
+from rentalos.db import get_db
+
+auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
 
 
-@auth_bp.route('/register', methods=('GET', 'POST'))
+@auth_bp.route("/register", methods=("GET", "POST"))
 def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
         db = get_db()
         error = None
 
         if not username:
-            error = '请输入用户名！'
+            error = "请输入用户名！"
         elif not password:
-            error = '请输入密码！'
+            error = "请输入密码！"
 
         if error is None:
             try:
-                auth_table = db.table('user')
-                if not auth_table.get(where('username') == username):
+                auth_table = db.table("user")
+                if not auth_table.get(where("username") == username):
                     userdata = {
-                        'username': username,
-                        'password': generate_password_hash(password)
+                        "username": username,
+                        "password": generate_password_hash(password),
                     }
                     auth_table.insert(userdata)
                 else:
-                    raise LookupError('username existed')
+                    raise LookupError("username existed")
             except LookupError:
-                error = f'用户:{username} 已经存在！'
+                error = f"用户:{username} 已经存在！"
             else:
-                return redirect(url_for('auth.login'))
+                return redirect(url_for("auth.login"))
 
         flash(error)
-    return render_template('auth/register.html')
+    return render_template("auth/register.html")
 
 
-@auth_bp.route('/login', methods=['GET', 'POST'])
+@auth_bp.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        auth_table = get_db().table('user')
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        auth_table = get_db().table("user")
         error = None
-        user = auth_table.get(where('username') == username)
+        user = auth_table.get(where("username") == username)
 
         if user is None:
-            error = f'该用户名:{username}不存在'
-        elif not check_password_hash(user['password'], password):
-            error = '该密码与用户名不匹配'
+            error = f"该用户名:{username}不存在"
+        elif not check_password_hash(user["password"], password):
+            error = "该密码与用户名不匹配"
 
         if error is None:
             session.clear()
-            session['user_doc_id'] = user.doc_id
-            return redirect(url_for('index'))
+            session["user_doc_id"] = user.doc_id
+            return redirect(url_for("index"))
 
         flash(error)
-    return render_template('auth/login.html')
+    return render_template("auth/login.html")
 
 
 @auth_bp.before_app_request
 def load_logged_in_user():
-    user_doc_id = session.get('user_doc_id')
-
+    user_doc_id = session.get("user_doc_id")
     if user_doc_id is None:
         g.user = None
     else:
-        g.user = get_db().get(doc_id=user_doc_id)
+        g.user = get_db().table("user").get(doc_id=user_doc_id)
 
 
-@auth_bp.route('/exit')
+@auth_bp.route("/exit")
 def exit():
     session.clear()
-    return redirect(url_for('auth.login'))
+    return redirect(url_for("auth.login"))
 
 
 def login_required(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
         if g.user is None:
-            return redirect('auth.login')
+            return redirect(url_for("auth.login"))
         return view(**kwargs)
 
     return wrapped_view
--- rentalos/manager.py
+++ rentalos/manager.py
@@ -1,15 +1,41 @@
-from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
-)
+from flask import Blueprint, flash, g, redirect, render_template, request, url_for
 from werkzeug.exceptions import abort
 
 from rentalos.auth import login_required
 from rentalos.db import get_db
-manager_bp=Blueprint('manager',__name__)
 
-@manager_bp.route('/')
+manager_bp = Blueprint("manager", __name__)
+
+
+@manager_bp.route("/")
 @login_required
 def index():
-    data_table=get_db().table('data')
-    datas=data_table.all()
-    return render_template("os/index.html",datas=datas)
+    data_table = get_db().table("data")
+    datas = data_table.all()
+    return render_template("os/index.html", datas=datas)
+
+
+@manager_bp.route("/add", methods=("GET", "POST"))
+@login_required
+def add():
+    if request.method == "POST":
+        carname = request.form["carname"]
+        status = request.form["status"]
+        count = request.form["count"]
+        error = None
+
+        if not carname:
+            error = "需要输入车辆名称"
+        elif not status:
+            error = "需要输入出借状态"
+        elif not count:
+            error = "需要输入出借次数"
+
+        if error is not None:
+            flash(error)
+        else:
+            data_table = get_db().table("data")
+            data_table.insert({"carname": carname, "status": status, "count": count})
+            return redirect(url_for("manager.index"))
+
+    return render_template("os/add.html")
