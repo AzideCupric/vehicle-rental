628c97cbf04982fbcbe4986816cf4d70c6bcac1e
Azide <rukuy@qq.com>
2022-06-12 01:44:51 +0800

    完善了租还车部分的添加功能

--- rentalos/carrental.py
+++ rentalos/carrental.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from flask import Blueprint, flash, g, redirect, render_template, request, url_for
 from pytz import timezone
 from tinydb import where
+from tinydb.operations import set
 from werkzeug.exceptions import abort
 
 from rentalos.auth import login_required
@@ -11,26 +12,21 @@ from rentalos.db import get_db
 carrental_bp = Blueprint("carrental", __name__, url_prefix="/carrental")
 
 
-@carrental_bp.route("/")
-@login_required
-def index():
-    carinfo_table = get_db().table("carinfo")
-    datas = carinfo_table.all()
-    return render_template("os/carrental/index.html", datas=datas, pagename="租还管理")
-
-
 def get_item(doc_id: int):
-    item = get_db().table("carrental").get(doc_id=doc_id)
+    raw_item = get_db().table("carinfo").get(doc_id=doc_id)
 
-    if item is None:
+    if raw_item is None:
         abort(404, f"不存在记录{doc_id}!")
+    else:
+        item = raw_item["rental"]
 
     return item
 
 
-def time_format(timestamp: int) -> str:
+def time_format2str(timestamp: int) -> str:
     dtime = datetime.fromtimestamp(timestamp, tz=timezone("Asia/Shanghai"))
-    timestr = dtime.strftime("%Y-%m-%d %H:%M:%S")
+    # dtime = datetime.strptime(time, '%Y-%m-%dT%H:%M')
+    timestr = dtime.strftime("%Y-%m-%d %H:%M")
     return timestr
 
 
@@ -39,5 +35,65 @@ def time_format(timestamp: int) -> str:
 def info(doc_id):
     items = get_item(doc_id)
     return render_template(
-        "os/carrental/info.html", items=items, time_format=time_format
+        "os/carrental/info.html",
+        items=items,
+        doc_id=doc_id,
+        time_format=time_format2str,
     )
+
+
+def time_format2stamp(timestr: str) -> int:
+    dtime = datetime.strptime(timestr, "%Y-%m-%dT%H:%M")
+    return int(dtime.timestamp())
+
+
+@carrental_bp.route("/<int:doc_id>/add", methods=("GET", "POST"))
+@login_required
+def add(doc_id):
+    if request.method == "POST":
+        user = request.form["user"]
+        starttime = time_format2stamp(request.form["start"])
+        stoptime = time_format2stamp(request.form["stop"])
+        cost = request.form["cost"]
+        error = None
+
+        if not user:
+            error = "需要输入租车人姓名"
+        elif not starttime:
+            error = "需要输入租车时间"
+        elif not stoptime:
+            error = "需要输入还车时间"
+        elif not cost:
+            error = "需要输入租金"
+
+        if error is None:
+            table = get_db().table("carinfo")
+            try:
+                items: dict = table.get(doc_id=doc_id)["rental"]
+                for idx, item in enumerate(items.values()):
+                    if (
+                        item["start"] <= starttime < item["stop"]
+                        or item["start"] <= stoptime < item["stop"]
+                        or starttime <= item["start"] < stoptime
+                        or starttime <= item["stop"] < stoptime
+                    ):
+                        raise LookupError(idx)
+            except LookupError as e:
+                idx = e.args[0]
+                error = f"当前时间与已有的第{idx}条记录冲突"
+            else:
+                new_item = {
+                    (len(items) + 1): {
+                        "user": user,
+                        "start": starttime,
+                        "stop": stoptime,
+                        "cost": cost,
+                    }
+                }
+                items.update(new_item)
+                table.update(set("rental", items), doc_ids=[doc_id])
+                return redirect(url_for("carrental.info", doc_id=doc_id))
+
+        flash(error)
+
+    return render_template("os/carrental/add.html")
